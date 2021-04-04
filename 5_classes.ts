8cbe169c770e439a27bb624c20c5b4ebba765ed6
allexib <ooomegasib@gmail.com>
2021-04-04 20:25:21 +0400

    classes

---
+++ 5_classes.ts
@@ -0,0 +1,24 @@
+class TypeScript {
+    version: string
+
+    constructor(version: string) {
+        this.version = version
+    }
+    info(name: string) {
+        return `[${name}]: Typescript version is ${this.version}`
+    }
+}
+
+// class Car {
+//     readonly model: string
+//     readonly numberOfWheels: number = 4
+//
+//     constructor(theModel: string) {
+//         this.model = theModel
+//     }
+// }
+
+class Car {
+    readonly numberOfWheels: number = 4
+    constructor(readonly model: string) {}
+}
