db9fdc98f02d3c805de929e2dc483f4ae060230e
allexib <ooomegasib@gmail.com>
2021-04-04 20:29:37 +0400

    modificators protected, public, private

--- 5_classes.ts
+++ 5_classes.ts
@@ -22,3 +22,21 @@ class Car {
     readonly numberOfWheels: number = 4
     constructor(readonly model: string) {}
 }
+//=======
+
+class Animal {
+    protected voice: string =''
+    public color: string = 'black'
+
+    private go() {
+        console.log('Go')
+    }
+}
+
+class Cat extends Animal {
+    public setVoice(voice: string): void {
+        this.voice = voice
+    }
+}
+
+const cat = new Cat()
