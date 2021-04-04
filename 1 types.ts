bad50ccb02872db139906a6976033962523d72a7
allexib <ooomegasib@gmail.com>
2021-04-04 19:33:49 +0400

    base type, tuple, any

--- 1 types.ts
+++ 1 types.ts
@@ -1,3 +1,23 @@
-const str: string = 'Hello'
+const isFetching: boolean = true
+const isLoading: boolean = false
 
-console.log(str)
+const int: number = 42
+const float: number = 4.2
+const num: number = 3a10
+
+const message: string = 'hello ts'
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const words: string[] = ['hello', 'ts']
+
+//tuple
+const contact: [string, number] = ['kama', 16781]
+
+// any
+let variable: any = 42
+// ...
+variable = 'new string'
+variable = []
+
